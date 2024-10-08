cf690049e2b286f5e9991e29fa10e72c0e96ec7d
krzysztofdanda <dandzior84@gmail.com>
2024-10-08 21:29:53 +0200

    warsztat

---
+++ js/script.js
@@ -0,0 +1,41 @@
+console.log('AJAX - Warsztat - pobierz dane po kliknieciu przycisku');
+
+let btnGetData = document.getElementById('get-data');
+const getData = () => {
+
+    fetch('https://akademia108.pl/api/ajax/get-post.php')
+    .then(res => res.json())
+    .then(data => {
+
+        let pId = document.createElement('p');
+        let pUserId = document.createElement('p');
+        let pTitleId = document.createElement('p');
+        let pBody = document.createElement('p');
+        let hr = document.createElement('hr');
+
+        pId.innerText = `Post ID: ${data.id}`;
+        pUserId.innerText = `User ID: ${data.userId}`;
+        pTitleId.innerText = `Title: ${data.title}`;
+        pBody.innerText = `Body: ${data.body}`;
+        
+
+        document.body.appendChild(pId);
+        document.body.appendChild(pUserId);
+        document.body.appendChild(pTitleId);
+        document.body.appendChild(pBody);
+        document.body.appendChild(hr);
+
+
+         //console.log(pId);
+          
+         })
+
+         .catch(error => {
+            console.error(error);
+         })
+
+    
+//console.log('getData()');
+}
+btnGetData.addEventListener('click', getData);
+
